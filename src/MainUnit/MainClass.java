321e2c50971fa2f2e5da5b619b6ed1359a8e73f7
Zatrasa <zatrasa@yandex.ru>
2021-06-29 17:32:10 +0300

    Zat3

--- src/MainUnit/MainClass.java
+++ src/MainUnit/MainClass.java
@@ -2,16 +2,16 @@ package MainUnit;
 
 public class MainClass {
     public static void main(String[] args) {
-        String S="Hello";
+        String S = "Hello";
         int i = 5;
         long l = 1234567890L;
         float f = 5.56F;
         double d = 0.45345345353453;
         char ch = 'J';
-        boolean b=true;
+        boolean b = true;
 
-        System.out.println(Calculate(1,1,1,1));
-        System.out.println(IsInInterval(4,5));
+        System.out.println(Calculate(1, 1, 1, 1));
+        System.out.println(IsInInterval(4, 5));
         PrintPositiv(-1);
         System.out.println(IsNegativ(-5));
         PrintName("Yura");
@@ -30,24 +30,30 @@ public class MainClass {
     }
 
     public static boolean IsInInterval(int a, int b) {
-        return 10<=(a+b)&&(a+b)<=20;
+        return 10 <= (a + b) && (a + b) <= 20;
     }
 
     public static void PrintPositiv(int a) {
-        if (a<0) { System.out.println("Отрицательное"); }
-        else { System.out.println("Положительное"); }
+        if (a < 0) {
+            System.out.println("Отрицательное");
+        } else {
+            System.out.println("Положительное");
+        }
     }
 
     public static boolean IsNegativ(int a) {
-        return a<0;
+        return a < 0;
     }
 
     public static void PrintName(String Nam) {
-        System.out.println("Привет, "+Nam+"!");
+        System.out.println("Привет, " + Nam + "!");
     }
 
     public static void TestYear(int Year) {
-        if (Year%4==0&&Year%100!=0||Year%400==0){System.out.println("Високосный");}
-        else {System.out.println("Не високосный");}
+        if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0) {
+            System.out.println("Високосный");
+        } else {
+            System.out.println("Не високосный");
+        }
     }
 }
